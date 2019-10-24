53869767b4cb8060207da80a4d19a01b30a41417
Mateus Braz <mateusbrazalmeida@hotmail.com>
2019-10-24 17:23:22 -0300

    Multiplo de 3

---
+++ Multiplo/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Multiplo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Digite um número: ");
+            int num = int.Parse(Console.ReadLine());
+
+            if (num / 3)
+            {
+                System.Console.WriteLine("É Multiplo de 3");
+            } else{
+                System.Console.WriteLine("Não é Multiplo de 3");
+            }
+        }
+    }
+}
