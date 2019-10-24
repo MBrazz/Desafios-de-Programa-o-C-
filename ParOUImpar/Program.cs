83c470eebcd129acf180a508abd03f2e2df0d909
Mateus Braz <mateusbrazalmeida@hotmail.com>
2019-10-24 17:24:14 -0300

    Par ou Impar

---
+++ ParOUImpar/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ParOUImpar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            System.Console.Write("Digite um número: ");
+            int num = int.Parse(System.Console.ReadLine());
+
+            if(num % 2 == 0){
+                System.Console.WriteLine("O número é Par");
+             } 
+             else {
+                 System.Console.WriteLine("O número é Ímpar");
+             }  
+        }
+    }
+}
