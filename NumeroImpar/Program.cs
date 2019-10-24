aef5b0968bf27e47dade04327cd4715725eb64d5
Mateus Braz <mateusbrazalmeida@hotmail.com>
2019-10-24 17:23:46 -0300

    Numero Impar de 0 a 10

---
+++ NumeroImpar/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace NumeroImpar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            for (int num = 1; num <= 10; num +=2 ){
+                Console.Write(num + " ");
+            }
+        }
+    }
+}
