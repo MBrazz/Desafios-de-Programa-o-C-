f45f6b9c3dc9675c06cbf65bf83869c0fc07f0a1
Mateus Braz <mateusbrazalmeida@hotmail.com>
2019-10-24 17:24:32 -0300

    Retangulo

---
+++ RetanCalc/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace RetanCalc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Qual a Base do Retângulo? ");
+            double baseRet = double.Parse(Console.ReadLine());
+            Console.Write("Qual a Altura do Retângulo? ");
+            double altRet = double.Parse(Console.ReadLine());
+            double areaRetan = (baseRet * altRet);
+            Console.WriteLine("A área do Retângulo é: " + areaRetan);
+            double periRetan = (2*(baseRet + altRet));
+            Console.WriteLine("O perimetro do Retângulo é: " + periRetan);
+
+        }
+    }
+}
