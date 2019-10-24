ff78936de63ef3b8448b2e562630587cdf54469d
Mateus Braz <mateusbrazalmeida@hotmail.com>
2019-10-24 17:23:05 -0300

    Conversão: Grau e Fahrenheit

---
+++ GrauFahrenheit/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace GrauFahrenheit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double TC = 0;
+            double TF = 0;
+
+            System.Console.WriteLine("Digite a Temperatura em Graus Celsius: ");
+            TC = double.Parse(System.Console.ReadLine());
+
+            TF = (TC/5)*9 + 32;
+
+            System.Console.WriteLine($"A Temperatura em Fahrenheit é: " + TF);
+        }
+    }
+}
