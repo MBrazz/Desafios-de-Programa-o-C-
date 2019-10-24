f4033525c33354088fb28497148e2964db4c350c
Mateus Braz <mateusbrazalmeida@hotmail.com>
2019-10-24 17:25:08 -0300

    Triangulo Invertido

---
+++ Triangulo/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Triangulo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            System.Console.Write("Digite a altura do Triângulo: ");
+            int k = int.Parse(System.Console.ReadLine());
+
+            for int i = k; 
+                for int j = 
+
+            System.Console.WriteLine("*");
+
+
+
+        }
+    }
+}
