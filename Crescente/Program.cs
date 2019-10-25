268af74b726110f2ca7493cbf54b9e7306a422e2
Mateus Braz <mateusbrazalmeida@hotmail.com>
2019-10-25 17:34:53 -0300

    Ordem Crescente

---
+++ Crescente/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Crescente
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+
+            Console.Write("Informe o primeiro número: ");
+            int num1 = int.Parse(Console.ReadLine());
+
+
+            Console.Write("Informe o segundo número: ");
+            int num2 = int.Parse(Console.ReadLine());
+
+
+            Console.Write("Informe o terceiro número: ");
+            int num3 = int.Parse(Console.ReadLine());
+
+            System.Console.WriteLine();
+
+            if (num3 < num1 && num3 > num2)
+            {
+                System.Console.WriteLine($"Ordem Crescente: {num2} {num3} {num1}");
+            }
+            else if (num3 > num1 && num3 < num2)
+            {
+               System.Console.WriteLine($"Ordem Crescente: {num1} {num3} {num2}");
+            }
+            else if (num1 > num2 && num1 > num3)
+            {
+                System.Console.WriteLine($"Ordem Crescente: {num3} {num2} {num2}");
+            }
+            else if (num3 > num2 && num2 > num1)
+            {
+                System.Console.WriteLine($"Ordem Crescente: {num1} {num2} {num3}");
+            }
+            else if (num1 > num2 && num2 < num3)
+            {
+                System.Console.WriteLine($"Ordem Crescente: {num1} {num2} {num3}");
+            }
+            else if (num2 > num1 && num2 > num3)
+            {
+                System.Console.WriteLine($"Ordem Crescente: {num3} {num1} {num2}");
+            }
+        }
+    }
+}
