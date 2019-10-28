8450c9bcf36785850a3b8a71f7be43bbcb78be90
Mateus Braz <mateusbrazalmeida@hotmail.com>
2019-10-27 21:34:19 -0300

    Triângulo

--- Triangulo/Program.cs
+++ Triangulo/Program.cs
@@ -6,17 +6,29 @@ namespace Triangulo
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Digite o 1º Lado do triângulo: ");
+            int lado1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o 2º Lado do triângulo: ");
+            int lado2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o 3º Lado do triângulo: ");
+            int lado3 = int.Parse(Console.ReadLine());
 
-            System.Console.Write("Digite a altura do Triângulo: ");
-            int k = int.Parse(System.Console.ReadLine());
-
-            for int i = k; 
-                for int j = 
-
-            System.Console.WriteLine("*");
-
-
-
+            if (lado1 + lado2 > lado3 )
+            {
+                Console.WriteLine("Lado 3 é menor que a soma do lado 1 e 2.");    
+            }
+            else if (lado3 + lado2 > lado1)
+            {
+                Console.WriteLine("Lado 1 é menor que a soma do lado 2 e 3.");
+            }
+            else if (lado3 + lado1 > lado2)
+            {
+                Console.WriteLine("Lado 2 é menor que a soma do lado 1 e 3.");
+            }
+            else
+            {
+                Console.WriteLine("Os lados não fazem parte de um triângulo.");
+            }
         }
     }
 }
