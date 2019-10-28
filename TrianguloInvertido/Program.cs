8424301d01bdbdcb1a53d33083f8e866d20beb2f
Mateus Braz <mateusbrazalmeida@hotmail.com>
2019-10-27 21:34:01 -0300

    Triângulo Invertido

--- TrianguloInvertido/Program.cs
+++ TrianguloInvertido/Program.cs
@@ -6,20 +6,15 @@ namespace Triangulo
     {
         static void Main(string[] args)
         {
-
-            System.Console.Write("Digite a altura do Triângulo: ");
-            int h = int.Parse(System.Console.ReadLine());
-
-            for (; h > 0; ++h)
-            {  
-                System.Console.WriteLine("*");
-                System.Console.WriteLine($" "+ h);
-            } 
-
+            Console.Write("Altura do Triângulo: ");
+            int tamanhoTriangulo = int.Parse (Console.ReadLine ());
             
-
-
-
-        }
+            Console.WriteLine();
+            string estrelas = "";
+            for (int j = 0; j < tamanhoTriangulo; j++) {
+                estrelas += "*";
+                Console.WriteLine (estrelas);
+            }
+        }   
     }
 }
